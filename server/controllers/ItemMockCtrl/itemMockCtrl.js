6ece8f27547ecfb6f73a6f25f156ea48861f193b
Chabaud <dsp_fx@hotmail.com>
2022-06-16 14:44:21 +0200

    TRAINING-4 refacto ramda fct

--- server/controllers/ItemMockCtrl/itemMockCtrl.js
+++ server/controllers/ItemMockCtrl/itemMockCtrl.js
@@ -3,13 +3,18 @@ var itemsList = require('./itemsList');
 
 const ItemMockCtrl = (() => {
   const createItem = item => {
-    item = R.compose(
-      R.assoc('_id', itemsList.length + 1),
-      R.assoc('createdAt', new Date()),
-      R.assoc('updatedAt', new Date())
+    return R.ifElse(R.isNil,
+      R.always(undefined),
+      () => {
+        item = R.compose(
+          R.assoc('_id', itemsList.length + 1),
+          R.assoc('createdAt', new Date()),
+          R.assoc('updatedAt', new Date())
+        )(item);
+        itemsList = R.concat(itemsList, [item]);
+        return item;
+      }
     )(item);
-    itemsList = R.concat(itemsList, [item]);
-    return item;
   };
 
   const getItemById = id => {
@@ -17,31 +22,36 @@ const ItemMockCtrl = (() => {
   };
 
   const updateItem = (id, itemToUpdate) => {
-    itemsList = R.map(item => {
-      if (item._id === id) {
-        const itemToAdd = R.compose(
-          R.assoc('_id', item._id),
-          R.assoc('createdAt', item.createdAt),
-          R.assoc('updatedAt', new Date())
-        )(itemToUpdate);
-        return itemToAdd;
-      } else return item;
-    }, itemsList);
-    return itemToUpdate;
+    R.ifElse(R.isNil,
+      R.always(undefined),
+      () => {
+        itemsList = R.map(
+          R.when(
+            R.propEq('_id', id),
+            R.compose(
+              R.assoc('updatedAt', new Date()),
+              R.mergeLeft(itemToUpdate)
+            )
+          ),
+          itemsList
+        );
+      }
+    )(itemToUpdate);
+
+    return R.find(R.propEq('_id', id))(itemsList);
   };
 
   const deleteItem = id => {
     const listLength = itemsList.length;
     itemsList = R.filter(item => item._id !== id, itemsList);
-    return listLength !== itemsList.lenth;
+    return listLength !== itemsList.length;
   };
 
   return {
     createItem,
     getItemById,
     updateItem,
-    deleteItem,
-    itemsList
+    deleteItem
   };
 })();
 
